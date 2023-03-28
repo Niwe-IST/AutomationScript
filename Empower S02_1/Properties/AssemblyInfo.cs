79bd6ec5b1baf884b9d23f0258ac8773b4f342b7
fejarque <ejarque.suscripciones@gmail.com>
2023-03-28 14:11:40 +0200

    Add project files.

---
+++ Empower S02_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Empower S02_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("Empower S02_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("B2284EB9-4FCA-49B1-9AA4-EEE9FA91F5DA")]
+[assembly: AssemblyVersion("1.0.0.0")]
